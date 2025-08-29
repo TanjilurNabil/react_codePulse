de1ba7dc5527df5a51c49028d02092bc2b9663d2
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2025-08-30 03:43:14 +0600

    auth service implemented

---
+++ src/services/authService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_URL = 'https://localhost:7176/api/Auth/';
+
+const register = (email, password) => {
+    return axios.post(API_URL + 'register', { email, password });
+};
+
+const login = (email, password) => {
+    return axios.post(API_URL + 'login', { email, password })
+        .then(response => {
+            if (response.data.token) {
+                localStorage.setItem('user', JSON.stringify(response.data));
+            }
+        });
+};
+
+const logout = () => { 
+    localStorage.removeItem('user');
+}
+const getCurrentUser = () => { 
+    return JSON.parse(localStorage.getItem('user'));
+}
+export default {
+    register,
+    login,
+    logout,
+    getCurrentUser
+}
