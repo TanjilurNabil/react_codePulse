268ad4a5293d27ef608623b96f63c7f3bb9dc466
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2025-08-30 04:01:39 +0600

    Login and Registration Component

---
+++ src/components/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authService from '../services/authService';
+
+const Login = () => {
+    const[email,setEmail]=useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    const hanldleLogin = (e) => {
+        e.preventDefault();
+        setMessage('');
+        authService.login(email, password).then(() => {
+            navigate('/categories');
+            window.location.reload();
+        },
+            (error) => {
+                const resMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+                setMessage(resMessage);
+            });
+    };
+    return (
+        <form onSubmit={hanldleLogin}>
+            <div>
+                <label>Email:</label>
+                <input type="text"value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            </div>
+            <button type='submit'>Login</button>
+            {message && <div className='error'>{ message}</div> }
+        </form>
+    );
+};
+
+export default Login;
---
+++ src/components/Register.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authService from '../services/authService';
+const Register = () => {
+    const[email,setEmail]=useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    const hanldleRegistration = (e) => {
+        e.preventDefault();
+        setMessage('');
+        authService.register(email, password).then(() => {
+            navigate('/categories');
+            window.location.reload();
+        },
+            (error) => {
+                const resMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+                setMessage(resMessage);
+            });
+    };
+    return (
+        <form onSubmit={hanldleRegistration}>
+            <div>
+                <label>Email:</label>
+                <input type="text"value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            </div>
+            <button type='submit'>Register</button>
+            {message && <div className='error'>{ message}</div> }
+        </form>
+    );
+};
+
+export default Register;
