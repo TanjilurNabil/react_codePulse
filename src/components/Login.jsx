0f6719a3cc3cc08e697c37140ceca909ab6fe1dc
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2025-09-01 00:32:31 +0600

    chore: update package dependencies and enhance Login component layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import authService from '../services/authService';
-
+// import * as yup from 'yup';
+// const schema = yup.object({
+//         email: yup.string().email('Invalid email format').required('Email is required'),
+//         password: yup.string().required('Password is required')
+//     })
+//         .required();
 const Login = () => {
     const[email,setEmail]=useState('');
     const [password, setPassword] = useState('');
@@ -19,8 +24,17 @@ const Login = () => {
                 setMessage(resMessage);
             });
     };
+    
+    
     return (
-        <form onSubmit={hanldleLogin}>
+        <div className='row mt-5 align-items-center justify-content-center'>
+            <div className='col-md-6 '>
+                <div className='card h-100'>
+                    <div className='card-header align-items-center'>
+                        <h2>Login</h2>
+                    </div>
+                    <div className='card-body d-flex flex-column'>
+<form onSubmit={hanldleLogin}>
             <div className='form-group'>
                 <label>Email:</label>
                 <input className='form-control' type="text"value={email} onChange={(e)=>setEmail(e.target.value)} required/>
@@ -29,12 +43,20 @@ const Login = () => {
                 <label>Password:</label>
                 <input className='form-control' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
             </div>
-            <div className='mt-2'>
-<button className='btn btn-primary ' type='submit'>Login</button>
+            <div className='mt-2 d-flex justify-content-center'>
+<button className='btn btn-primary' type='submit'>Login</button>
             </div>
             
             {message && <div className='error'>{ message}</div> }
         </form>
+                    </div>
+                </div>
+                    
+            </div>
+        
+            
+        </div>
+        
     );
 };
 
